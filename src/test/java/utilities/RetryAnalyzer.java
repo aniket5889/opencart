83bbd2f398fe91de2f508b110a65ba3c28988ed0
aniket5889 <tanwar.aniket98@gmail.com>
2025-12-27 11:22:47 +0530

    Add .gitignore for Selenium automation project cleanup

---
+++ src/test/java/utilities/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int maxRetry = 2;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetry) {
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+}
